d79ddfc023d636504e52f2eb65391b88972953ac
Ubuntu <MEAdmin1@ME-LNX-1.clhdpjfo0aeuvcaut1epyvvlib.bx.internal.cloudapp.net>
2024-02-01 13:32:47 +0000

    Changes

--- diabetes_insight_project/settings.py
+++ diabetes_insight_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8^*guq#jv0+&o&i62s-+28x2^s=0ri#8aw29#z#u2g^#xs&k_v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.0.5","20.62.66.163"]
+ALLOWED_HOSTS = ["10.0.0.5","datasciencedemo.kredservices.com","20.168.202.214","4.157.95.115"]
 
 
 # Application definition
